c4890537fe70cfdf52ed9443c041e702ff358a51
Kerriea-star <ojared511@gmail.com>
2023-07-28 06:40:10 +0300

    Index-Based For loops

--- control_flow.py
+++ control_flow.py
@@ -25,3 +25,33 @@ j = 0
 while j < len(data) and data[j] != "X":
     j += 1
 
+# For loops
+total = 0
+for val in data:
+    total += val # note use of the loop variable val
+
+
+biggest = data = [0] # as we assume nonempty list
+for val in data:
+    if val > biggest:
+        biggest = val
+
+# we can use a for loop in cases for which a while loop does not
+# apply, such as when iterating through a collection of, such as
+# a set, that does not support any direct form of indexing
+
+# Index-Based For Loops
+
+# In some applications, we need knowledge of the index of an
+# element within the sequence. For example, suppose that we need
+# to know where the maximum element in a list resied.
+
+# Rather than directly looping over the elements of the list in that case,
+# we prefer to loop over all possible indices of the list.
+# For this purpose, Python provides a built-in class called "range"
+# that generates "integer sequences".
+
+big_index = 0
+for j in range(len(data)):
+    if data[j] > data[big_index]:
+        big_index = j
