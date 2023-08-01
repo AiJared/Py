b11a19df9d8e4fdf3a4407ee27972b5736511863
Kerriea-star <ojared511@gmail.com>
2023-08-01 05:53:37 +0300

    inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,16 @@
+"""
+A hierarchical design is useful in software development, as common functionality
+can be grouped at the most general level, thereby promoting reuse of code,
+while differentiated behaviors can be viewd as extensions of the general case, In
+object-oriented programming, the mechanism for a modular and hierachical organization
+is a technique known as "inheritance". This allows a new class to be defined
+based upon an existing class as the starting point. In object-oriented terminology,
+the existing class is typically described as the "base class", "parent class", or "super
+class", while the newly defined class is known as the "subclass" or "child class".
+
+There are two ways in which a subclass can differentiate itself from its 
+superclass. A subclass may "specialize" an existing behavior by providing a new
+implementation and "overrides" an exisiting method. A subclass may also "extend" its
+superclass by providing brand new methods.
+"""
+
