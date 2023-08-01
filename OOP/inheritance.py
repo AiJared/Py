5a1379b535e7fb34f8dfab1dbe19b70b6df81aa5
Kerriea-star <ojared511@gmail.com>
2023-08-01 06:14:04 +0300

    inheritance

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -25,3 +25,74 @@ classes to denote errors that may occur in the context of their application. Tho
 user-defined exception types should be declared as subclasses of Exception
 """
 
+# Extending the CreditCard Class
+
+"""
+To demonstrate the mechanisms for inheritance in Python, we revisit the CreditCard
+class, implementing a subclass that, for lack of a better name, we
+name PredatoryCreditCard. The new class will differ from the original in two
+ways: (1) if an attempted charge is rejected because it would have exceeded the
+credit limit, a $5 fee will be charged and (2) there will be a mechanism for assessing
+a monthly interest charge on the outstanding balance, based up on Annual
+Percentage Rate(APR) specified as a constructor parameter.
+
+In accomplishing this goal, we demonstrate the techbique of specialization
+and extension. To charge a fee for an invalid charge attempt, we "override" the
+existing charge methods, thereby specializing it to provide the new functionality
+(although the new version takes advantage of a call to the overridden version). To
+provide support for charging interest, we extend the class with a new method named
+process_month
+"""
+class CreditCard:
+    """A consumer credit card"""
+
+    def __init__(self, customer, bank, acnt, limit):
+        """Create a new credit card instance
+        
+        The initial balance is zero.
+
+        customer    the name of the customer(e.g., "John Bowman")
+        bank        the name of the bank(e.g., "California Savings")
+        acnt        the acount identifier(e.g., "5391 0375 9387 5309")
+        limit       credit limit (measured in dollars)
+        """
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        """Return name of the customer."""
+        return self._customer
+    
+    def get_bank(self):
+        """Return the bank's name."""
+        return self._bank
+    
+    def get_account(self):
+        """Return the card identifying number(typically stored as a string)."""
+        return self._account
+    
+    def get_limit(self):
+        """Return current credit limit."""
+        return self._limit
+    
+    def get_balance(self):
+        """Return current balance."""
+        return self._balance
+    
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient credit limit.
+        
+        Return True if charge was processed; False if charge was denied.
+        """
+        if price + self._balance > self._limit: # if charge would exceed limit
+            return False
+        else:
+            self._balance += price
+            return True
+        
+    def make_payment(self, amount):
+        """Process customer payment that reduces balance."""
+        self._balance -= amount
