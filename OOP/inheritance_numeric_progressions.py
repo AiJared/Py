0dd862c6f7c7888dbcff85a023018ce64af0194c
Kerriea-star <ojared511@gmail.com>
2023-08-02 05:19:40 +0300

    Testing Numeric Progression Classes

--- OOP/inheritance_numeric_progressions.py
+++ OOP/inheritance_numeric_progressions.py
@@ -202,3 +202,22 @@ our progression classes and the code below it shows the output of that test.
 if __name__ == "__main__":
     print("Default progression:")
     Progression().print_progression(10)
+
+    print("Arithmetic progression with increment 5:")
+    ArithmeticProgression(5).print_progression(10)
+
+    print("Arithmetic progression with increment 5 and start 2:")
+    ArithmeticProgression(5,2).print_progression(10)
+
+    print("Geometric progression with default base:")
+    GeometricProgression().print_progression(10)
+
+    print("Geometric progression with base 3:")
+    GeometricProgression(3).print_progression(10)
+
+    print("Fibonacce progression with default start values:")
+    FibonacciProgression().print_progression(10)
+
+    print("Fibonacci progression with start values 4 and 6:")
+    FibonacciProgression(4,6).print_progression(10)
+    
