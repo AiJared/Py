68b9f78dc13503c6e197e362c184e3625c056081
Kerriea-star <ojared511@gmail.com>
2023-08-02 06:53:48 +0300

    inheritance, abstract base class

--- OOP/inheritance_abstract_base_classes.py
+++ OOP/inheritance_abstract_base_classes.py
@@ -68,3 +68,39 @@ class Sequence(metaclass=ABCMeta):
             if self[j] == val:          # found a match
                 k += 1
         return k
+    
+"""
+The implementation relies on two advanced Python techniques. The first is that
+we declare the ABCMeta class of the abc module as a "metaclass" of our Sequence
+class. A metaclass is different from a superclass, in that it provides a template for
+the class definition itself. Specifically, the ABCMeta declaration assures that the
+constructor for the class raises an error.
+
+The second advanced technique is the use of the @abstractmethod decorator
+immediately before the __len__ and __getitem__ methods are declared. That
+declares these two particular methods to be abstract, meaning that we do not provide
+an implemetation within our Sequence base class, but that we expect any concrete
+subclasses to support those two methods. Python enforces this exception, by disallowing
+instantiation for any subclass that does not override the abstract methods
+with concrete implementation.
+
+The rest of the Sequence class definition provides tangible implementations for
+other behaviors, under the assumption that the abstract __len__ and __getitem__
+methods will exist in a concrete subclass. If you carefully examine the source code,
+the implemetation of methods __contains__, index and count do not rely on any
+assumption about the self instances, other than syntax len(self) and self[j] are
+supported (by special methods __len__ and __getitem__, respectively). Support 
+for iteration is automatic.
+
+Finally, we emphasize that if a subclass provides its own implemetation of 
+an inherited behavior from a base class, the new definition overrides the inherited
+one. This technique can be used when we have the ability to provide a more efficient
+implemetation for a behavior than is achieved by the generic approach. As
+an example, the general implemetation of __contains__ for a sequence is based
+on loop used to search for the desired value. For our Range class, there is an 
+opportunity for a more efficient determination of containment. For example, it
+is evident that the expression, 100000 in Range(0, 2000000, 100), should evaluate
+to True, even without examining the individual elements of the range, because
+the range include 100000, as that is a multiple of 100 that is between the start and 
+stop values.
+"""
