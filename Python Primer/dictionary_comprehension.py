4f31228ee38b424410792bd41f734db2754af102
Kerriea-star <ojared511@gmail.com>
2023-08-06 12:11:57 +0300

    dictionary comprehension

--- Python Primer/dictionary_comprehension.py
+++ Python Primer/dictionary_comprehension.py
@@ -7,3 +7,16 @@ each item being a pair of a number and its square.
 squares = {x: x*x for x in range(6)}
 print(squares)
 
+"""The above code is equivalent to:"""
+
+squares = {}
+for x in range(6):
+    squares[x] = x*x
+
+"""
+A dictionary comprehension can optionally contain more for or if statements. An optional if
+statement can filter out items to form the new dictionary. Here are some examples to make
+with only odd items.
+"""
+odd_squares = {x: x*x for x in range(11) if x%2 == 1}
+print(odd_squares)
