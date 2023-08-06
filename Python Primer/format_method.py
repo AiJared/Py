a71621bdbf4b884ef4182a80d2ddff188141ad35
Kerriea-star <ojared511@gmail.com>
2023-08-06 09:04:06 +0300

    the format method

--- Python Primer/format_method.py
+++ Python Primer/format_method.py
@@ -13,3 +13,22 @@ print("{1}, {0} and {2}".format("John", "Bill", "Sean"))
 
 # order using keyword arguments
 print("{s}, {b} and {j}".format(j="John", b="Bill", s="Sean"))
+
+"""
+The format method can have optional format specifications. They are separated from field
+name using colon. For example, we can left-justify <, right-justify >, or center ^ a string in the
+given space. We can also format integers as binary, hexadecimal etc. and floats can be rounded
+or displayed in the exponent format.
+"""
+
+# formating integers
+print("Binary representation of {0} is {0:b}".format(12))
+
+# formatting floats
+print("Exponent representation: {0:e}".format(1566.345))
+
+# round off
+print("One third is {0:.3f}".format(1/3))
+
+# string alignment
+print("|{:<10}| {:^10}| {:>10}".format("butter", "bread", "ham"))
