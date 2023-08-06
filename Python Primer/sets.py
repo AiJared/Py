bb71c10b40df1112a2e3f519b866783097a90bc8
Kerriea-star <ojared511@gmail.com>
2023-08-06 10:44:09 +0300

    python frozenset

--- Python Primer/sets.py
+++ Python Primer/sets.py
@@ -130,3 +130,21 @@ G = A ^ B
 print(G)
 G = A.symmetric_difference(B)
 print(G)
+
+# Python Frozenset
+"""
+Frozenset is a new class that has the characteristics of a set, but its elements cannot be changed
+once assigned. While tuples are immutable lists, frozensets are immutable sets. Sets being
+mutable are unhashable, so they can't be used as dictionary keys. On the other hand, frozensets
+are hashable and can be used as keys to a dictionary.
+
+Frozensets can be created using the function frozenset(). This datatype supports methods like
+copy(), difference(), intersection(), isdisjoint(), issubset(), issuperset(),
+symmetric_difference() and union(). Being immutable it does not have method that add or
+remove elements.
+"""
+A = frozenset([1, 2, 3, 4])
+B = frozenset([3, 4, 5, 6])
+print(A.isdisjoint(B))
+
+print(A.difference(B))
