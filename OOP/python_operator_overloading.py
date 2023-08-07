24d811a6f715a5f5328612b3ea671cce7ae89fe0
Kerriea-star <ojared511@gmail.com>
2023-08-07 11:57:03 +0300

    special functions

--- OOP/python_operator_overloading.py
+++ OOP/python_operator_overloading.py
@@ -7,14 +7,14 @@ different meaning according to the context is called operator overloading.
 So what happens when we use them with objects of a user-defined class? Let us consider the
 following class, which tries to simulate a point in 2-D coordinate system.
 """
-class Point:
-    def __init__(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x = 0, y = 0):
+#         self.x = x
+#         self.y = y
 
 """Now when we try to add two points that we create as follows."""
-p1 = Point(2, 3)
-p2 = Point(-1, 2)
+# p1 = Point(2, 3)
+# p2 = Point(-1, 2)
 # print(p1 + p2)
 
 """
@@ -32,3 +32,31 @@ functions in Python
 Using special functions, we can make our class compatible with built-in functions
 """
 
+# p1 = Point(2, 3)
+# print(p1)
+
+"""
+That did not print well. But if we define __str__() method in our class, we can control how it gets printed.
+"""
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+p1 = Point(2, 3)
+print(p1) 
+
+"""
+That's better. Turns out that this same method is invoked when we use the built-in function
+str() or format()
+"""
+print(str(p1))
+print(format(p1))
+
+"""
+So when you do str(p1) or format(p1), Python is internally doing p1.__str__(). Hence the
+name, special functions.
+"""
