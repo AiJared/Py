48252bbc4414859af2911be9f40380bf21020c83
Kerriea-star <ojared511@gmail.com>
2023-08-07 12:34:44 +0300

    Python Operator Overloading

--- OOP/python_operator_overloading.py
+++ OOP/python_operator_overloading.py
@@ -88,3 +88,33 @@ print(p1 + p2)
 What actually happens is that, when you do p1 + p2, Python will call p1.__add__(p2) which in
 turn is Point.__add__(p1,p2). Similarly, we can overload other operators as well.
 """
+
+# Overloading Comparison Operators in Python
+"""
+Python does not limit operator overloading to arithmetic operators only. We can overload
+comparison operators as well. Suppose we wanted to implement less than symbol <
+in our Point class. Let us compare the magnitude of these points from the origin and return the
+result for this purpose. It can be implemented as follows.
+"""
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+    
+    def __lt__(self, other):
+        self_mag = (self.x ** 2) + (self.y ** 2)
+        other_mag = (other.x ** 2) + (other.y ** 2)
+        return self_mag < other_mag
+    
+"""Sample runs"""
+print(Point(1, 1) < Point(-2, -3))
+print(Point(1, 1) < Point(0.5, -0.2))
+print(Point(1, 1) < Point(1, 1))
