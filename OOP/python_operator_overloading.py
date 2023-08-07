4d975d12d2fb02cf812e76d154b39833bd38599a
Kerriea-star <ojared511@gmail.com>
2023-08-07 12:04:26 +0300

    overloading the + operator

--- OOP/python_operator_overloading.py
+++ OOP/python_operator_overloading.py
@@ -60,3 +60,31 @@ print(format(p1))
 So when you do str(p1) or format(p1), Python is internally doing p1.__str__(). Hence the
 name, special functions.
 """
+
+# Overloading the + operator
+"""
+To overload + sign, we will need to implement __add__() function in the class. We will
+return a Point object of the coordinate sum.
+"""
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+
+# now lets try that addition again.
+p1 = Point(2, 3)
+p2 = Point(-1, 2)
+print(p1 + p2)
+
+"""
+What actually happens is that, when you do p1 + p2, Python will call p1.__add__(p2) which in
+turn is Point.__add__(p1,p2). Similarly, we can overload other operators as well.
+"""
