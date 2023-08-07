ade6999b564a5a4a7f8c938984e4d151522acd67
Kerriea-star <ojared511@gmail.com>
2023-08-07 09:36:15 +0300

    method overriding

--- OOP/inheritance_polygon.py
+++ OOP/inheritance_polygon.py
@@ -38,3 +38,31 @@ t = Triangle()
 t.inputSides()
 t.dispSides()
 t.findArea()
+
+# Method Overriding
+"""
+In the above example, notice that __init__(), method was defined in both classes, Triangle as
+well as in Polygon. when this happens, the method in the derived class overrides that in the base
+class. This is to say, __init__() in Triangle gets preference over the same in Polygon.
+Generally when overriding a base method, we tend to extend the definition rather than simply
+replace it. The same is being done by calling the method in base class from the one in derived
+class (calling Polygon.__init__() from __init__() in Triangle). A better option would be
+to use the built-in function super(). So, super().__init__(3) is equivalent to
+Polygon.__init__(self, 3) and is preferred.
+
+Two built-in functions isinstance() and issubclass() are used to check inheritances.
+Function isintance() returns True if the object is an instance of the class or other classes
+derived from it. Each and every class in Python inherits from the base class object.
+"""
+
+print(isinstance(t, Triangle))
+print(isinstance(t, Polygon))
+print(isinstance(t, int))
+print(isinstance(t, object))
+
+"""
+Similarly, issubclass is used to check for class inheritance
+"""
+print(issubclass(Polygon, Triangle))
+print(issubclass(Triangle, Polygon))
+print(issubclass(bool, int))
