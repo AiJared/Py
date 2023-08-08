d8a707adc1a2c316eac763ba2a9a447762c46988
Kerriea-star <ojared511@gmail.com>
2023-08-08 09:55:56 +0300

    Decorators

--- python_decorators.py
+++ python_decorators.py
@@ -60,3 +60,52 @@ Finally, we must know about closures in Python.
 """
 
 # Back to Decorators
+"""
+Functions and methods are called "callable" as they can be called. In fact, any object which
+implements the special method __call__() is termed callable. So, in the most basic sense, a
+decorator is a callable that returns a callable. Basically, a decorator takes in a function, adds
+some functionality and returns it.
+"""
+def make_pretty(func):
+    def inner():
+        print("I got decorated")
+        func()
+    return inner
+
+def ordinary():
+    print("I am ordinary")
+
+ordinary()
+
+"""Let's decorate this ordinary function"""
+pretty = make_pretty(ordinary)
+pretty()
+
+"""
+In the example shown above, make_pretty() is a decorator. In the assignmet step
+pretty = make_pretty(ordinary)
+
+The function ordinary() got declared and the returned function was given the name pretty.
+We can see that the decorator function added some new functionality to the original function.
+This is similar to packing a gift. The decorator acts a wrapper. The nature of the object that
+got decorated(actual gift inside) does not alter. But now, it looks pretty(since it got decorated).
+
+Generally, we decorate a function and reassign it as, 
+"""
+ordinary = make_pretty(ordinary)
+
+"""
+This is a common construct and for this reason, Python has a syntax to simplify this. We can use
+the @ symbol along with the name of the decorator function and place it above the definition of
+the function to be decorated. For example.
+"""
+@make_pretty
+def ordinary():
+    print("I am ordinary")
+
+"""is equivalent to"""
+def ordinary():
+    print("I am ordinary")
+ordinary = make_pretty(ordinary)
+
+"""This is just a syntactic sugar to implement decorators."""
