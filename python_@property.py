94f444178307410aa59cbbfcb1909574c04af320
Kerriea-star <ojared511@gmail.com>
2023-08-08 15:15:31 +0300

    getters and setters

--- python_@property.py
+++ python_@property.py
@@ -50,3 +50,46 @@ asked us to implement this value constraint. Being a comapny that strive for cus
 satisfaction, we heeded the suggestion and released version 1.01, an upgrade of our
 existing class.
 """
+# Using Getters and Setters
+
+"""
+An obvious solution to the above constraint will be to hide the attribute temperature (make it
+private) and define new getter and setter interfaces to manipulate it. This can be done as follows.
+"""
+class Celsius:
+    def __init__(self, temperature = 0):
+        self.set_temperature(temperature)
+
+    def to_fahrenheit(self):
+        return (self.get_temperature() * 1.8) + 32
+    
+    # new update
+    def get_temperature(self):
+        return self._temperature
+    
+    def set_temperature(self, value):
+        if value < -273:
+            raise ValueError("Temperature below -273 is not possible")
+        self._temperature = value
+
+"""
+We can see that new methods get_temperature() and set_temperatures() were
+defined and furthermore, temperature was replaced wih _temperature. An underscore(_)at
+the beginning is used to denote protected variables in Python.
+"""
+# c = Celsius(-277)
+# print(c)
+
+c = Celsius(37)
+print(c.get_temperature())
+c.set_temperature(10)
+# c.set_temperature(-300)
+"""
+The big problem with the above update is that, all the clients
+who implemented our previous class in their program have to modify theier code from
+obj.temperature to obj.get_temperature() and all assignments like obj.temperature = 
+val to obj.set_temperature(val). This refactoring can cause headaches to the clients with
+hundreds of thousands of lines of codes.
+
+All in all, our new update was not backward compatible. This is why property comes to rescue.
+"""
