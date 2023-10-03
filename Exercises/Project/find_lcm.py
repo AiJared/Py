863928c8d2ba9f3afc34ba38ae55f20b00e024d8
Kerriea-star <ojared511@gmail.com>
2023-10-03 09:08:13 +0300

    LCM

--- Exercises/Project/find_lcm.py
+++ Exercises/Project/find_lcm.py
@@ -41,3 +41,43 @@ and beyond. In each iteration, we check if both input numbers perfectly divides 
 If so, we store the number as LCM and break from the loop. Otherwise, the number is incremented
 by 1 and the loop continues.
 """
+
+"""
+The above program is slower to run. we can make it more efficient by using the fact thay the
+product of two numbers is equal to the product of least common multiple and greatest common
+divisor of those two numbers.
+
+Number1 * Number2 = LCM * GCD.
+
+Here is a Python program to implement this.
+"""
+
+# define gdc function
+def gcd(x, y):
+    """
+    This function implements euclidean algorithm
+    to find the GCD
+    """
+    while (y):
+        x, y = y, x % y
+    return x
+
+# define lcm function
+def lcm(x, y):
+    """This function takes two 
+    integers and returns the LCM."""
+    lcm = (x*y) // gcd(x, y)
+    return lcm
+
+# take input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The LCM of", num1, "and", num2, "is", lcm(num1, num2))
+
+"""
+The output of this program is same as before. We have two functions gcd() and lcm(). We
+require GCD of the numbers to calculate its LCM. So, lcm() calls the function gcd() to
+accomplish this. GCD of two numbers can be calculated efficiently using the Euclidean
+algorithm.
+"""
